bac9122e5baf96d6ce2791ea9839e3a7b7d5f908
GitHub <noreply@github.com>
2020-08-03 09:11:23 +0000

    Updates

---
+++ component---src-pages-404-jsx-262ced493ff26271b931.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{pssB:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),u=n.n(a),l=n("I/Ru"),o=n("jNNy");t.default=function(){return u.a.createElement(l.a,null,u.a.createElement(o.a,{title:"404: Not found",location:"/404"}),u.a.createElement("h1",null,"NOT FOUND"),u.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-jsx-262ced493ff26271b931.js.map
